76c71a37c9e1f89e2ef506d9500ce9053607f9e5
Ilya Ivanov <static.ila@gmail.com>
2015-08-19 15:17:03 +0300

    Finished with collapsing menu

---
+++ js/collapse-menu.js
@@ -0,0 +1,16 @@
+(function ($){
+    $(document).on("click", "a[data-collapse-target]", function () {
+        var sourceElement = $(this).attr('data-collapse-target');
+
+        var destinationElement = $('[data-collapse-menu="' + sourceElement + '"]');
+
+        var destinationElementGroup = destinationElement.attr('data-collapse-group');
+
+        var openElementsInGroup = $('.in[data-collapse-group="' + destinationElementGroup + '"]');
+
+        if(openElementsInGroup.length > 0)
+            openElementsInGroup.collapse('hide');
+
+        destinationElement.collapse('toggle');
+    });
+})(jQuery);
